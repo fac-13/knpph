2a7be702e5685203f0869d3dccaf8e7f6ee08539
missKatiaPunter <katiapunter@gmail.com>
2018-03-15 12:45:32 +0000

    adds string template loop to make a list of actors Relates #21

--- design.js
+++ design.js
@@ -5,7 +5,7 @@ var designFunctions = {
             <div class="content-holder">
                 <h2>Overview</h2>
                 <p>${jsonIN.plot}</p>
-
+                ${jsonIN.actors.map(actor => `<li>${actor}</li>`).join(' ')}
             </div>`;
         return markup;
     }
